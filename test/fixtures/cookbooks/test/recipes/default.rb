fd01536676acb6f5bbb6a7a70f090589d5c89beb
Bastian Schwarz <bastian@codename-php.de>
2021-07-03 13:29:42 +0200

    Installing additional packages since these are not installed by the code package. Those seem to be x11 packages

--- test/fixtures/cookbooks/test/recipes/default.rb
+++ test/fixtures/cookbooks/test/recipes/default.rb
@@ -1,2 +1,11 @@
+user 'create user 1' do
+  manage_home true
+  username 'user1'
+end
+user 'create user 2' do
+  manage_home true
+  username 'user2'
+end
+
 codenamephp_vscode_repository 'Install apt repo'
 codenamephp_vscode_package 'Install vscode'
